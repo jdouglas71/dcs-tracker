ccfb57689c4b8ec0c75c11a0e2b0b2ca8d014a7a
Jason Douglas <jdouglas71@gmail.com>
2018-05-30 11:40:30 -0700

    anon ip google analytics

--- dcs-tracker.php
+++ dcs-tracker.php
@@ -77,13 +77,16 @@ function dcs_tracker_google_tracking_code()
             var _gaq = _gaq || [];
             _gaq.push(
                 ['_setAccount', '" . esc_js( $google_analytics_id ) . "'],
-                ['_trackPageview']
+                ['_trackPageview'],
+                ['_gat._anonymizeIp']
             );
 
             (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                var ga = document.createElement('script'); 
+                ga.type = 'text/javascript'; 
+                ga.async = true;
                 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); 
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); 
             })();
 
         </script>";

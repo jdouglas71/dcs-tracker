b879a49519cf503f02f809fa65b9f12fed1ad460
Jason Douglas <jdouglas71@gmail.com>
2020-03-07 08:49:11 -0800

    Cleanup for RTI

--- dcs-tracker-admin.php
+++ dcs-tracker-admin.php
@@ -64,27 +64,26 @@ function dcs_tracker_admin_page()
 	$retval .= "<div class='wrap'>";
 	
 	$retval .= '<h2 class="nav-tab-wrapper">';
-    $retval .= '<a href="?page=dcs_tracker&tab=reference-codes" class="nav-tab '.(($active_tab=='reference-codes')?'nav-tab-active':'').'">Reference Codes</a>';
-    $retval .= '<a href="?page=dcs_tracker&tab=agent-portal" class="nav-tab '.(($active_tab=='agent-portal')?'nav-tab-active':'').'">Agent Portals</a>';
-    $retval .= '<a href="?page=dcs_tracker&tab=google-opts" class="nav-tab '.(($active_tab=='google-opts')?'nav-tab-active':'').'">Google Options</a>';
+    $retval .= '<a href="?page=dcs_tracker&tab=reference-codes" class="nav-tab '.(($active_tab=='reference-codes')?'nav-tab-active':'').'">Offer Codes</a>';
+    //$retval .= '<a href="?page=dcs_tracker&tab=google-opts" class="nav-tab '.(($active_tab=='google-opts')?'nav-tab-active':'').'">Google Options</a>';
 	$retval .= '</h2>';
 	
 	if( $active_tab == "reference-codes" )
 	{
 		if( isset($_GET['created']) )
 		{
-			$status = "The Reference Code has been created.";
+			$status = "The Offer Code has been created.";
 		}
 	
 		if( isset($_GET['updated']) )
 		{
-			$status = "The Reference Code has been updated.";
+			$status = "The Offer Code has been updated.";
 		}
 
 		$ref_codes = get_option("dcs_tracker_discounts", array());
 		ksort($ref_codes);
 
-		$retval .= "<h1>Reference Codes/MegaPortals</h1>";
+		$retval .= "<h1>Offer Codes</h1>";
 		$retval .= "<hr class='dcs-tracker-line'>";
 		
 		if( $status == NULL )
@@ -99,13 +98,9 @@ function dcs_tracker_admin_page()
 		
 		$retval .= "<div class='dcs-tracker-code'>";
 		$retval .= "<table>";
-		$retval .= "<tr><td><label for='dcs-tracker-code-name'>Reference Code</label></td><td><input name='dcs-tracker-code-name' id='dcs-tracker-code-name'></td></tr>";
-		$retval .= "<tr><td><label for='dcs-tracker-allow-international'>Allow International TI</label></td><td><input type='checkbox' name='dcs-tracker-allow-international' id='dcs-tracker-allow-international'></td></tr>";
+		$retval .= "<tr><td><label for='dcs-tracker-code-name'>Offer Code</label></td><td><input name='dcs-tracker-code-name' id='dcs-tracker-code-name'></td></tr>";
 		$retval .= "<tr><td><label for='dcs-tracker-code-value'>Discount Value ($)</label></td><td><input name='dcs-tracker-code-value' id='dcs-tracker-code-value'></td></tr>";
-		$retval .= "<tr><td><label for='dcs-tracker-code-type'>Type</label></td><td><select name='dcs-tracker-code-type' id='dcs-tracker-code-type'><option value='flat'>Discount</option><option value='percentage'>Percentage</option><option value='flat_rate'>Flat Rate</option></select></td></tr>";
-		$retval .= "<tr><td><label for='dcs-tracker-code-create-page'>Create Page</label></td><td style='text-align:right;'><input type='checkbox' name='dcs-tracker-code-create-page' id='dcs-tracker-code-create-page'></td></tr>";
-		$retval .= "<tr id='dcs-tracker-code-redirect-page-parent' style='display:none;'><td><label for='dcs-tracker-code-redirect-parent'>Parent Redirect Page (portal)</label></td><td><input type='checkbox' name='dcs-tracker-code-redirect-parent' id='dcs-tracker-code-redirect-parent'></td></tr>";		
-		$retval .= "<tr id='dcs-tracker-code-redirect-page' style='display:none;'><td><label for='dcs-tracker-code-redirect'>Redirect Page</label></td><td><input name='dcs-tracker-code-redirect' id='dcs-tracker-code-redirect'></td></tr>";		
+		$retval .= "<tr><td><label for='dcs-tracker-code-type'>Type</label></td><td><select name='dcs-tracker-code-type' id='dcs-tracker-code-type'><option value='percentage'>Percentage</option><option value='flat_rate'>Flat Rate</option></select></td></tr>";
 		$retval .= "<tr><td></td><td style='text-align:right;'><input type='submit' id='dcs-tracker-create-code' value='Create Code'></td></tr>";
 		$retval .= "</table>";
 		$retval .= "</div>";
@@ -113,12 +108,12 @@ function dcs_tracker_admin_page()
 		$retval .= "<div class='dcs-tracker-ref-codes'>";
 		if( $ref_codes == NULL )
 		{
-			$retval .= "<h2>No Reference Codes Defined.</h2>";		
+			$retval .= "<h2>No Offer Codes Defined.</h2>";		
 		}
 		else
 		{
 			$retval .= "<table class='dcs-tracker-ref-codes'>";
-			$retval .= "<tr><th>Delete</th><th>Reference Code</th><th>Allow International TI</th><th>Discount Type</th><th>Discount</th><th>Has Page?</th><th>Redirect Page</th><th>Landing Page URL</th></tr>";
+			$retval .= "<tr><th>Delete</th><th>Offer Code</th><th>Discount Type</th><th>Discount</th></tr>";
 			foreach($ref_codes as $name => $values)
 			{ 
 				$retval .= "<tr>";
@@ -126,13 +121,6 @@ function dcs_tracker_admin_page()
 
 				$retval .= "<td>".$name."</td>";
 				
-				$allow_international = "false";
-				if( isset($values['allow_international']) )
-				{
-					$allow_international = $values['allow_international'];
-				}
-				$retval .= "<td>".$allow_international."</td>";
-								
 				if( $values['amount'] != '' )
 				{
 					$retval .= "<td>".$values['type']."</td>";
@@ -157,25 +145,6 @@ function dcs_tracker_admin_page()
 				{
 					$retval .= "<td></td>";
 				}
-				$has_page = "false";
-				if( isset($values['has_page']) ) $has_page = $values['has_page'];
-				$retval .= "<td>".$has_page."</td>";
-				
-				$redirect = "";
-				if( isset($values['redirect']) ) $redirect = $values['redirect'];
-				if( $redirect == '' ) $redirect = "Home";
-				$retval .= "<td>".$redirect."</td>";
-				
-				$use_portal_parent = "false";
-				if( isset($values['use_portal_parent']) ) $use_portal_parent = $values['use_portal_parent'];
-				$parent_page = "/";
-				if( $use_portal_parent == "true" ) $parent_page = "/portal/";
-				
-				if( $has_page == "true" )
-					$retval .= "<td><a href='".site_url($parent_page.$name)."'>".site_url($parent_page.$name)."</a></td>";
-				else
-					$retval .= "<td></td>";
-				$retval .= "</tr>";
 			}
 			$retval .= "<tr><td colspan=6 style='padding-top:100px;text-align:right;'><input type='submit' id='dcs-tracker-update-code' value='Update'></td></tr>";
 
@@ -183,72 +152,6 @@ function dcs_tracker_admin_page()
 			$retval .= "</div>";
 		}
 	}	
-	else if( $active_tab == "agent-portal" )
-	{
-		if( isset($_GET['created']) )
-		{
-			$status = "The Agent Portal has been created.";
-		}
-	
-		if( isset($_GET['updated']) )
-		{
-			$status = "The Agent Portal has been updated.";
-		}
-		
-		if( isset($_GET['error']) )
-		{
-			$status = $_GET['error_message'];
-		}
-
-		$agent_portals = get_option("dcs_agent_portals", array());
-		ksort($agent_portals);
-		$retval .= "<h1>Agent Portals</h1>";
-		$retval .= "<hr class='dcs-tracker-line'>";
-		
-		$retval .= "<div class='dcs-tracker-code'>";
-		$retval .= "<table>";
-		$retval .= "<tr><td><label for='dcs-tracker-agent-name'>Name</label></td><td><input name='dcs-tracker-agent-name' id='dcs-tracker-agent-name'></td></tr>";
-		$retval .= "<tr><td><label for='dcs-tracker-agent-filter'>Agent Filter</label></td><td><input name='dcs-tracker-agent-filter' id='dcs-tracker-agent-filter'></td></tr>";
-		$retval .= "<tr><td><label for='dcs-tracker-allow-international'>Allow International TI</label></td><td><input type='checkbox' name='dcs-tracker-allow-international' id='dcs-tracker-allow-international'></td></tr>";
-		$retval .= "<tr><td></td><td style='text-align:right;'><input type='submit' id='dcs-tracker-create-agent-portal' value='Create Agent Portal'></td></tr>";
-		$retval .= "</table>";
-		$retval .= "</div>";
-
-		$retval .= "<div class='dcs-tracker-ref-codes'>";
-		if( $agent_portals == NULL )
-		{
-			$retval .= "<h2>No Agent Portals Defined.</h2>";		
-		}
-		else
-		{
-			$retval .= "<table class='dcs-tracker-ref-codes'>";
-			$retval .= "<tr><th>Delete</th><th>Name</th><th>Agent Filter</th><th>Allow International TI</th><th>Portal URL</th></tr>";
-			foreach($agent_portals as $name => $values)
-			{ 
-				$retval .= "<tr>";
-				$retval .= "<td><input type='checkbox' class='dcs-tracker-agent-delete' value='".$name."'></td>";
-				$retval .= "<td>".$name."</td>";
-				
-				$retval .= "<td>".$values['agent_filter']."</td>";
-
-				$allow_international = "false";
-				if( isset($values['allow_international']) )
-				{
-					$allow_international = $values['allow_international'];
-				}
-				$retval .= "<td>".$allow_international."</td>";
-
-				
-				$retval .= "<td><a href='".site_url("/portal/".$name)."'>".site_url("/portal/".$name)."</a></td>";
-
-				$retval .= "</tr>";
-			}
-			$retval .= "<tr><td colspan=4 style='text-align:right;padding-top:100px;'><input type='submit' id='dcs-tracker-update-agent-portal' value='Update'></td></tr>";
-			
-			$retval .= "</table>";
-			$retval .= "</div>";
-		}
-	}
 	else if( $active_tab == "google-opts" )
 	{
 		$google_analytics_flag = get_option( "dcs_tracker_google_analytics_flag" );
@@ -282,12 +185,8 @@ function dcs_tracker_create_code()
 
 	//Do stuff here
 	$name = strtolower($_POST['name']);
-	$allow_international = $_POST['allow_international'];
 	$amount = $_POST['amount'];
 	$type = $_POST['type'];
-	$redirect = $_POST['redirect'];
-	$has_page = $_POST['has_page'];
-	$use_portal_parent = $_POST['use_portal_parent'];
 	$status = "";
 
 	if( $type == "percentage" )
@@ -303,128 +202,13 @@ function dcs_tracker_create_code()
 								   "use_portal_parent" => $use_portal_parent );
 
 	update_option( "dcs_tracker_discounts", $discountArray );
-		
-	if( $has_page == "true" )
-	{
-		//Create portal parent page if necessary
-		$portal_page = get_page_by_title( 'portal' );
-		$portal_page_id = 0;
-		if( $use_portal_parent == "true" )
-		{
-			if( $portal_page == NULL )
-			{
-				$portal_post = array( 
-					'post_title' => "portal",
-					'post_status' => 'publish',
-					'post_author' => get_current_user_id(),
-					'post_type' => 'page',
-				);
-				$portal_page_id = wp_insert_post( $portal_post );
-			}
-			else
-			{
-				$portal_page_id = $portal_page->ID;
-			}
-		}
-	
-		//Does page with this title already exist?
-		$page = get_page_by_title( $name );
-		
-		$my_post = array(
-			'post_title'    => wp_strip_all_tags( $name ),
-			'post_content'  => '[dcs_tracker_landing_page tracking_id="'.$name.'" redirect_page="'.$redirect.'" allow_international_ti="'.$allow_international.']',
-			'post_status'   => 'publish',
-			'post_author'   => get_current_user_id(),
-			'post_type'     => 'page',
-			'post_parent'   => $portal_page_id,
-			'page_template' => "page-templates/page_fullwidth.php",
-		);
-		
-		// Insert the post into the database
-		$result = wp_insert_post( $my_post, true );
-		if( !is_wp_error($result) )
-		{
-			$status = "&created=1";
-			$discountArray[$name]["ID"] = $result;
-		}
-		else
-		{
-			$status = "&error=1?error_message=".$result->get_error_message();
-		}	
-	}
-	
-	update_option( "dcs_tracker_discounts", $discountArray );
-	
+
 	echo wp_get_referer().$status;
 	//echo "https://espn.com";
 	die();
 }
 add_action( 'wp_ajax_dcs_tracker_create_code', 'dcs_tracker_create_code' );
 add_action( 'wp_ajax_nopriv_dcs_tracker_create_code', 'dcs_tracker_create_code' );
-
-/**
-* Create Agent Portal.
-*/
-function dcs_tracker_create_agent_portal()
-{
-	check_ajax_referer( "dcs_tracker_create_agent_portal", "dcs_tracker_create_agent_portal_nonce" );
-
-	$agent_portals = get_option("dcs_agent_portals", array());
-
-	//Do stuff here
-	$name = strtolower($_POST['name']);
-	$agent_filter = strtolower($_POST['agent_filter']);
-	$allow_international = $_POST['allow_international'];
-	$status = "";
-
-	$agent_portals[$name] = array( "agent_filter" => $agent_filter, 
-								   "allow_international" => $allow_international, 	
-								 );
-								 
-	update_option( "dcs_agent_portals", $agent_portals );
-	
-	//Create portal parent page if necessary
-	$portal_page = get_page_by_title( 'portal' );
-	$portal_page_id = 0;
-	if( $portal_page == NULL )
-	{
-		$portal_post = array( 
-			'post_title' => "portal",
-			'post_status' => 'publish',
-			'post_author' => get_current_user_id(),
-			'post_type' => 'page',
-		);
-		$portal_page_id = wp_insert_post( $portal_post );
-	}
-	else
-	{
-		$portal_page_id = $portal_page->ID;
-	}
-	
-	//Does page with this title already exist?
-	$page = get_page_by_title( $name );
-	
-	$my_post = array(
-		'post_title'    => wp_strip_all_tags( $name ),
-		'post_content'  => '[ripcord_quote_machine agent_filter="'.$agent_filter.'" allow_international_ti="'.$allow_international.'"]',
-		'post_status'   => 'publish',
-		'post_author'   => get_current_user_id(),
-		'post_type'     => 'page',
-		'post_parent'   => $portal_page_id,
-		'page_template' => "page-templates/page_fullwidth.php",
-	);
-	
-	// Insert the post into the database
-	$post_id = wp_insert_post( $my_post );
-	$status = "&created=1";
-	$agent_portals[$name]['ID'] = $post_id;
-	
-	echo wp_get_referer().$status;
-	
-	die();
-}
-add_action( 'wp_ajax_dcs_tracker_create_agent_portal', 'dcs_tracker_create_agent_portal' );
-add_action( 'wp_ajax_nopriv_dcs_tracker_create_agent_portal', 'dcs_tracker_create_agent_portal' );
 
 /**
 * Update Codes 
@@ -450,31 +234,6 @@ function dcs_tracker_update_codes()
 }
 add_action( 'wp_ajax_dcs_tracker_update_codes', 'dcs_tracker_update_codes' );
 add_action( 'wp_ajax_nopriv_dcs_tracker_update_codes', 'dcs_tracker_update_codes' );
-
-/**
-* Update Agents 
-*/
-function dcs_tracker_update_agents()
-{
-	check_ajax_referer( "dcs_tracker_update_agents", "dcs_tracker_update_agents_nonce" );
-	$agent_portals = get_option("dcs_agent_portals", array());
-
-	$values = $_POST['values'];
-	$vals = explode( ";", $values );
-	foreach( $vals as $name )
-	{
-		unset($agent_portals[$name]);
-	}
-	
-	update_option( "dcs_agent_portals", $agent_portals );
-
-	$status = "&updated=1";
-
-	echo wp_get_referer().$status;
-	die();
-}
-add_action( 'wp_ajax_dcs_tracker_update_agents', 'dcs_tracker_update_agents' );
-add_action( 'wp_ajax_nopriv_dcs_tracker_update_agents', 'dcs_tracker_update_agents' );
 
 /**
 * Update Google Options 
